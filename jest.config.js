fb4738f6bf1034f747f0501a5f961d3076f4f2b7
William Dunkerley <hahaveryfun@gmail.com>
2020-04-03 20:12:52 -0400

    feat(jest): Add file transformation for jest for static file imports

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,8 @@ module.exports = {
 	testRegex: '__test__/.*\\.test\\.js$',
 	testPathIgnorePatterns: ['<rootDir>/node_modules/'],
 	setupFilesAfterEnv: ["<rootDir>/__test__/setupJest.js"],
+	moduleNameMapper: {
+		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/fileTransformer.js",
+		"\\.(css|less)$": "identity-obj-proxy"
+	}
 }
