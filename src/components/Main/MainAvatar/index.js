77e6b9935dba1ca228a39f6aa10764de06c5347c
William Dunkerley <hahaveryfun@gmail.com>
2020-07-09 23:15:54 -0400

    fix(MainAvatar): Correct profile image back to github

--- src/components/Main/MainAvatar/index.js
+++ src/components/Main/MainAvatar/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Avatar} from '@material-ui/core';
 import {useStyles} from './styles';
 
+// const img = "http://dripbox-qa.williamdunkerley.com/drip/a664c077-990e-406b-a459-dcbc541e7a77";
 const img = "https://avatars3.githubusercontent.com/u/6320828?s=460&u=3711ba34e57e482444bfccb4c5992024e6b4b134&v=4";
 
 const MainAvatar = (props) => {
