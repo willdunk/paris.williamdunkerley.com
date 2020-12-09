64ee6700821d073197b718f5ca1a6e52cc73e7ca
William Dunkerley <hahaveryfun@gmail.com>
2020-12-08 18:19:13 -0600

    feat(Nav): Add mobile-friendly navigation

--- src/components/common/Layout/styles.js
+++ src/components/common/Layout/styles.js
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles(theme => ({
 	nav: {
 		margin: `${theme.spacing(3)}px ${0}px ${theme.spacing(4)}`,
+		[theme.breakpoints.down('xs')]: {
+			marginBottom: theme.spacing(0),
+		},
 	},
 	title: {
 		color: theme.palette.primary.contrastText,
