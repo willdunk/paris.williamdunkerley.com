8aa74e24f23394725a6a4d1626b5817240d8b2a2
William Dunkerley <hahaveryfun@gmail.com>
2020-12-16 22:56:47 -0600

    feat(LoginForm): Add redux-form login skeleton

--- src/components/Login/LoginForm/index.js
+++ src/components/Login/LoginForm/index.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form'
+import { Field, reduxForm, submit} from 'redux-form'
+import {Card, Grid, TextField, CardContent, Typography, Button} from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+const FORM_NAME = 'login';
+
+const ReduxTextField = ({
+	label,
+	input,
+	meta: { touched, invalid, error },
+	...custom
+}) => (
+		<TextField
+			label={label}
+			placeholder={label}
+			error={touched && invalid}
+			helperText={touched && error}
+			{...input}
+			{...custom}
+		/>
+	)
 
 const LoginReduxForm = reduxForm({
-	form: 'login'
+	form: FORM_NAME
 })(props => {
 	const { handleSubmit } = props;
+	const dispatch = useDispatch();
 	return (
-		<form onSubmit={handleSubmit}>
-			<div>
-				<label htmlFor="firstName">First Name</label>
-				<Field name="firstName" component="input" type="text" />
-			</div>
-			<div>
-				<label htmlFor="lastName">Last Name</label>
-				<Field name="lastName" component="input" type="text" />
-			</div>
-			<div>
-				<label htmlFor="email">Email</label>
-				<Field name="email" component="input" type="email" />
-			</div>
-			<button type="submit">Submit</button>
-		</form>
+		<Grid
+			container
+			justify="center"
+		>
+			<Grid
+				item
+				xs={12}
+				md={4}
+			>
+				<Card raised={true}>
+					<CardContent>
+						<Typography gutterBottom variant="h5" component="h5">
+							Login
+						</Typography>
+						<form onSubmit={handleSubmit}>
+							<Grid
+								container
+								spacing={2}
+							>
+								<Grid
+									item
+									xs={12}
+								>
+									<Field
+										name="username"
+										component={ReduxTextField}
+										label="Username"
+										variant="outlined"
+										fullWidth={true}
+									/>
+								</Grid>
+								<Grid
+									item
+									xs={12}
+								>
+									<Field
+										name="password"
+										component={ReduxTextField}
+										label="Password"
+										variant="outlined"
+										type="password"
+										fullWidth={true}
+										autoComplete="current-password"
+									/>
+								</Grid>
+							</Grid>
+							<Button
+								onClick={() => dispatch(submit(FORM_NAME))}
+							>
+								Login
+							</Button>
+						</form>
+					</CardContent>
+				</Card>
+			</Grid>
+		</Grid>
 	);
 })
 
